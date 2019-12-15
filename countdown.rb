9e5a5422ccdbd72ae2f543de3e5b84b8856f8bb2
Taishawn King <taishawnking1@yahoo.com>
2019-12-15 08:47:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,13 +4,28 @@ def countdown
   x = 12
   while x > 0 
   puts "#{x} SECOND(S)!"
+  x -= 1 
+  if x ==0
+    puts "HAPPY NEW YEAR!"
+   
+  end
+ end
+end
+return countdown
+
+
+
+def countdown_with_sleep
+  x = 12
+  while x > 0 
+  puts "#{x} SECOND(S)!"
 sleep (1)
   x -= 1 
   if x ==0
     puts "HAPPY NEW YEAR!"
    
+  end
  end
-end
 end
 return countdown
 
